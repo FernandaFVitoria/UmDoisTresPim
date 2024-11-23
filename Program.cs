6c12f3dfd2a6a40534745b524745dd92c7b681d9
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-11-22 21:47:45 -0300

    resolvido

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for (int i = 1; i <= 40; i++)
+        {
+            // Verifica se o número atual é múltiplo de 4.
+            if (i % 4 == 0)
+            {
+                Console.Write("pim");
+            }
+            else
+            {
+                Console.Write(i);
+            }
+ 
+            // Adiciona um espaço após cada número ou palavra, exceto no final da linha.
+            if (i % 10 != 0)
+            {
+                Console.Write(" ");
+            }
+            else
+            {
+                // Quebra a linha após 10 números/palavras.
+                Console.WriteLine();
+            }
+        }
